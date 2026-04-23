8d2df1b480c12b3c6f2624786a44eafe4115e7b0
Jaasiel Beltrán <jaasiel0303@gmail.com>
2026-04-23 06:08:34 -0400

    feat: agregando funxiones de agregar y borrar tareas

--- src/context/useListaStore.ts
+++ src/context/useListaStore.ts
@@ -1,11 +1,59 @@
 import { create } from 'zustand'
-import type { ListaItem } from '../types/store'
+import type { ListaItem, TaskItem } from '../types/store'
+
+const toggleTaskInLists = (
+  listas: ListaItem[],
+  listaId: string,
+  taskId: string,
+): ListaItem[] => {
+  return listas.map((lista) => {
+    if (lista.id !== listaId) return lista
+
+    return {
+      ...lista,
+      task: lista.task.map((task) =>
+        task.id === taskId ? { ...task, status: !task.status } : task,
+      ),
+    }
+  })
+}
+const addTareaInList = (
+  listas: ListaItem[],
+  listaID: string,
+  newtask: TaskItem,
+): ListaItem[] => {
+  return listas.map((val) => {
+    if (val.id !== listaID) return val
+
+    return {
+      ...val,
+      task: [...val.task, newtask],
+    }
+  })
+}
+
+const removeTareaInList = (
+  listas: ListaItem[],
+  listaID: string,
+  taskID: string,
+): ListaItem[] => {
+  return listas.map((val) => {
+    if (val.id !== listaID) return val
+
+    return {
+      ...val,
+      task: val.task.filter((el) => el.id !== taskID),
+    }
+  })
+}
 
 interface ListaStore {
   lista: ListaItem[]
   addLista: (value: ListaItem) => void
   removeLista: (value: string) => void
   toggleTaskStatus: (listaId: string, taskId: string) => void
+  addTarea: (listaId: string, newTarea: TaskItem) => void
+  removeTarea: (listaId: string, taskID: string) => void
 }
 
 export const useListaStore = create<ListaStore>((set) => ({
@@ -20,16 +68,17 @@ export const useListaStore = create<ListaStore>((set) => ({
   },
   toggleTaskStatus: (listaId, taskId) => {
     set((state) => ({
-      lista: state.lista.map((lista) =>
-        lista.id === listaId
-          ? {
-              ...lista,
-              task: lista.task.map((task) =>
-                task.id === taskId ? { ...task, status: !task.status } : task,
-              ),
-            }
-          : lista,
-      ),
+      lista: toggleTaskInLists(state.lista, listaId, taskId),
+    }))
+  },
+  addTarea: (listaId, newTarea) => {
+    set((state) => ({
+      lista: addTareaInList(state.lista, listaId, newTarea),
+    }))
+  },
+  removeTarea: (listaId, taskID) => {
+    set((state) => ({
+      lista: removeTareaInList(state.lista, listaId, taskID),
     }))
   },
 }))
